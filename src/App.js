f40cdb0665e37c950880e33406c3400fe1d68641
YonatanEini <55375356+YonatanEini@users.noreply.github.com>
2022-08-19 11:14:49 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect} from 'react';
+import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import React,{Component} from "react";
+import { Grid } from './main-page';
+
+function App() {
+
+      return(
+
+        <Grid>
+
+        </Grid>
+        
+        );
+
+  }
+
+
+  
+
+
+
+
+ 
+
+
+
+export default App;
